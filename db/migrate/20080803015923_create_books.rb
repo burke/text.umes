4ff6c3d3fd0b3365df5ecd0c57bb78eab35afeac
Burke Libbey <burke@burkelibbey.org>
2008-08-10 15:31:18 -0700

    Initial Commit

---
+++ db/migrate/20080803015923_create_books.rb
@@ -0,0 +1,18 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :seller
+      t.integer :price
+      t.text :comments
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
